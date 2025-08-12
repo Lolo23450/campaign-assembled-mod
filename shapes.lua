f66cb8590900f200f5b6407e7a5eb6af32911891
Lolo23450 <91489171+Lolo23450@users.noreply.github.com>
2025-08-12 10:29:45 -0400

    Tinkrell Overhaul

--- shapes.lua
+++ shapes.lua
@@ -2,19 +2,22 @@
 	{40359001
 		{
 			{
-				verts={{15, -5}, {-15, -5}, {15, 5}}
+				verts={{-20, 10}, {20, 10}, {20, -10}, {-20, -10}}
 				ports={
-					{0, 0.17}
-					{0, 0.5}
-					{0, 0.83}
-					{1, 0.18}
-					{1, 0.5}
-					{1, 0.82}
-					{2, 0.5}
+					{0, 0.125}    -- First port at 1/8
+					{0, 0.375}    -- Second port at 3/8
+					{0, 0.625}    -- Third port at 5/8
+					{0, 0.875}    -- Fourth port at 7/8
+					{1, 0.25}     -- First port at 1/4
+					{1, 0.75}     -- Second port at 3/4
+					{2, 0.125}    -- First port at 1/8
+					{2, 0.375}    -- Second port at 3/8
+					{2, 0.625}    -- Third port at 5/8
+					{2, 0.875}    -- Fourth port at 7/8
+					{3, 0.25}    -- First port at 1/4
+					{3, 0.75}     -- Second port at 3/4
 				}
 			}
 		}
 	}
-
-	{40359002, {}, mirror_of=40359001},
 }
